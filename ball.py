d181eeabcdaf592b41e0702783d129cc3a3b6d02
tessachang <91043767+tessachang@users.noreply.github.com>
2022-05-03 02:13:09 -0500

    Add files via upload
    Hypothesis 3

--- ball.py
+++ ball.py
@@ -1,24 +1,11 @@
 """
 Ball.py
 ---------
-A custom class specific for bouncing_ball.py
+A custom class specific for bouncing_ball_hp2.py
 """
 import random
 from tkinter import Canvas
-
 import numpy as np
-
-POPULATION = 5
-W_WIDTH = 800
-W_HEIGHT = 600
-BALL_OFFSET = 1
-BALL_SIZE = 10
-X_SPEED = 3
-Y_SPEED = 3
-FRAME_RATE = 1 / 240
-INFECTED_CASE = 2
-MASK_PROTECTION_RATE = 0.66
-
 
 
 def get_random_num(min_int, max_int, exclude_zero=True):
@@ -29,7 +16,12 @@ def get_random_num(min_int, max_int, exclude_zero=True):
 
 
 class Ball:
-    def __init__(self, canvas, x, y, diameter, x_speed, y_speed, color, mask, vaccine):
+
+    infected = []
+    recovered = []
+    ball_position = []
+
+    def __init__(self, canvas, x, y, diameter, x_speed, y_speed, color, mask=False, vaccine=False):
         # self.ball = None
         # self.y_coord = None
         # self.x_coord = None
@@ -38,30 +30,16 @@ class Ball:
         self.canvas = canvas
         self.x = x
         self.y = y
+        if x>500:
+            self.section = 'right'
+        else:
+            self.section = 'left'
         self.diameter = diameter
-        self.image = canvas.create_oval(x, y, x + diameter, y + diameter, fill=color, width=0)
+        self.image = canvas.create_oval(x, y, x + diameter, y + diameter, fill=color, width=2, outline='#F5F5F5')
         self.x_speed = x_speed
         self.y_speed = y_speed
         self.mask = mask
         self.vaccine = vaccine
-
-        # self.ball_position = []
-        # for i in range(POPULATION + INFECTED_CASE):
-        #     self.x_coord, self.y_coord = get_random_num(0, W_WIDTH, False), get_random_num(0, W_HEIGHT, False)
-        #     if i <= POPULATION:
-        #         self.ball_position.append = Canvas.create_oval(self.canvas,
-        #                                                        self.x_coord,
-        #                                                        self.y_coord,
-        #                                                        self.x_coord + self.diameter,
-        #                                                        self.y_coord + self.diameter,
-        #                                                        fill=HEALTHY, width=0)
-        #     else:
-        #         self.ball_position.append = Canvas.create_oval(self.canvas,
-        #                                                        self.x_coord,
-        #                                                        self.y_coord,
-        #                                                        self.x_coord + self.diameter,
-        #                                                        self.y_coord + self.diameter,
-        #                                                        fill=INFECTED, width=0)
 
     def move(self):
         # Boundary collision detection
@@ -71,9 +49,4 @@ class Ball:
         if (max_y >= self.canvas.winfo_height()-1 and self.y_speed > 0) or (min_y < 0 and self.y_speed < 0):
             self.y_speed = -self.y_speed
         self.canvas.move(self.image, self.x_speed, self.y_speed)
-
-    def ball_collide(self):
-        pass
-
-
 
