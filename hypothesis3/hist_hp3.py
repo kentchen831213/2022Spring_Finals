3b650bc2b3b32cfa56aafcdd6e23f986f5d6431c
tessachang <91043767+tessachang@users.noreply.github.com>
2022-05-08 11:46:21 -0500

    Add files via upload

--- hypothesis3/hist_hp3.py
+++ hypothesis3/hist_hp3.py
@@ -2,26 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-def making_hist(csv_name):
-    df=pd.read_csv(csv_name, usecols=['rate'])
 
+def making_hist(csv_name) -> None:
+    """plotting the histogram of infected rate for the simulations
+    :param csv_name: the csv file that store the information for each simulation
+    :return: None
+    """
+    df = pd.read_csv(csv_name, usecols=['rate'])
 
     print("Statistics of the simulation: ")
     print(df.describe())
 
-    plt.hist(df['rate'],bins=10)
+    plt.hist(df['rate'], bins=10)
     plt.show()
 
 
-def computing_mean(csv_name):
-    df=pd.read_csv(csv_name, usecols=['recovered'])
-
+def computing_mean(csv_name) -> None:
+    """calculating the mean total infected people for all simulations
+    :param csv_name: the csv file that store the information for each simulation
+    :return: None
+    """
+    df = pd.read_csv(csv_name, usecols=['recovered'])
 
     print("The mean total infected people for each simulation is: ", df.mean().values[0])
 
 
 if __name__ == '__main__':
-
     file = sys.argv[1]
     computing_mean(file)
     making_hist(file)
